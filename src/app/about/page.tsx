362f19f3e0f9ae8c9cfb67af183ecebc47e0bd95
zx3923 <seop3923@gmail.co>
2024-07-25 15:33:01 +0900

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,18 @@
+import styles from "../styles/about.module.css";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <h1>About us</h1>
+        <div>
+          <span>
+            Welcome to the officail explorer for The New York Times Best Seller
+            list explorer.
+          </span>
+          <span>We hope you enjoy your stay!</span>
+        </div>
+      </div>
+    </div>
+  );
 }

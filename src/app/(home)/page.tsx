c2494c219722ef007e1fa65f126517f421493470
zx3923 <seop3923@gmail.co>
2024-07-25 16:08:18 +0900

    getAPI all best sellers

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,21 @@
 import styles from "../styles/home.module.css";
 
-export default function Home() {
-  return <div>home</div>;
+const API_KEY = process.env.API_KEY;
+
+async function getBestSellers() {
+  const response = await fetch(`${API_KEY}`);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Home() {
+  const sellers = await getBestSellers();
+  console.log(sellers);
+  return (
+    <div>
+      {sellers.results.map((item: any) => (
+        <div key={item.list_name}>{item.list_name}</div>
+      ))}
+    </div>
+  );
 }
